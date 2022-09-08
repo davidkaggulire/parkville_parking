01ada1a232844f9332ab18285616af7a20b99a6e
davidkaggulire1 <dkaggulire@gmail.com>
2022-09-08 13:05:52 +0300

    configuration file

---
+++ config.py
@@ -0,0 +1,39 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Config(object):
+    DEBUG = False
+    TESTING = False
+    CSRF_ENABLED = True
+    SECRET_KEY = os.environ['SECRET_KEY']
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+
+class StagingConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class DevelopmentConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    TESTING = True
+
+
+app_config = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'production': ProductionConfig,
+    'staging': StagingConfig
+}
