6f873d9c659a53c292e54916876beccc3ff7d78d
davidkaggulire1 <dkaggulire@gmail.com>
2022-09-10 13:06:44 +0300

    add serializers

--- api/serializers/charge_serializer.py
+++ api/serializers/charge_serializer.py
@@ -1,16 +1,106 @@
-from ..models import Charge
+from ..models import Bodacharge, Carcharge, Coastercharge, Taxicharge, Truckcharge
 
-def response_serializer(charges: Charge):
+
+def truck_serializer(charges: Truckcharge):
     response = []
 
     for charge in charges:
-        charge_dict= {
+        charge_dict = {
             'id': str(charge.id),
-            'day_charge': charge.day_charge,
-            'night_charge': charge.night_charge,
-            'hour_charge': charge.hour_charge,
+            "duration": charge.duration,
+            'charge': charge.charge
         }
 
         response.append(charge_dict)
 
-    return response
+    final_output = {
+        "status": "success",
+        "results": len(response),
+        "truck_charges": response
+    }
+
+    return final_output
+
+
+def taxi_serializer(charges: Taxicharge):
+    response = []
+
+    for charge in charges:
+        charge_dict = {
+            'id': str(charge.id),
+            "duration": charge.duration,
+            'charge': charge.charge
+        }
+
+        response.append(charge_dict)
+
+    final_output = {
+        "status": "success",
+        "results": len(response),
+        "taxi_charges": response
+    }
+
+    return final_output
+
+
+def coaster_serializer(charges: Coastercharge):
+    response = []
+
+    for charge in charges:
+        charge_dict = {
+            'id': str(charge.id),
+            "duration": charge.duration,
+            'charge': charge.charge
+        }
+
+        response.append(charge_dict)
+
+    final_output = {
+        "status": "success",
+        "results": len(response),
+        "coaster_charges": response
+    }
+
+    return final_output
+
+
+def car_serializer(charges: Carcharge):
+    response = []
+
+    for charge in charges:
+        charge_dict = {
+            'id': str(charge.id),
+            "duration": charge.duration,
+            'charge': charge.charge
+        }
+
+        response.append(charge_dict)
+
+    final_output = {
+        "status": "success",
+        "results": len(response),
+        "car_charges": response
+    }
+
+    return final_output
+
+
+def boda_serializer(charges: Bodacharge):
+    response = []
+
+    for charge in charges:
+        charge_dict = {
+            'id': str(charge.id),
+            "duration": charge.duration,
+            'charge': charge.charge
+        }
+
+        response.append(charge_dict)
+
+    final_output = {
+        "status": "success",
+        "results": len(response),
+        "boda_charges": response
+    }
+
+    return final_output
