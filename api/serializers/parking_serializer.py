d0494d152c7a8f6816b8fda33383224e2d7b3d51
davidkaggulire1 <dkaggulire@gmail.com>
2022-09-11 03:01:43 +0300

    payment serializer

---
+++ api/serializers/parking_serializer.py
@@ -0,0 +1,111 @@
+from ..models import PaymentBodaboda, PaymentCar, PaymentCoaster, PaymentTaxi, PaymentTruck
+
+
+def car_parking_serializer(payments: PaymentCar):
+    response = []
+
+    for payment in payments:
+
+        payment_dict = {
+            "id": str(payment.id),
+            "vehicle_id": payment.vehicle_id,
+            "charge_id": payment.charge_id,
+        }
+
+        response.append(payment_dict)
+
+    final_output = {
+        "status": "success",
+        "results": len(response),
+        "payments": response
+    }
+
+    return final_output
+
+
+def coaster_parking_serializer(payments: PaymentCoaster):
+    response = []
+
+    for payment in payments:
+
+        payment_dict = {
+            "id": str(payment.id),
+            "vehicle_id": payment.vehicle_id,
+            "charge_id": payment.charge_id,
+        }
+
+        response.append(payment_dict)
+
+    final_output = {
+        "status": "success",
+        "results": len(response),
+        "payments": response
+    }
+
+    return final_output
+
+
+def truck_parking_serializer(payments: PaymentTruck):
+    response = []
+
+    for payment in payments:
+
+        payment_dict = {
+            "id": str(payment.id),
+            "vehicle_id": payment.vehicle_id,
+            "charge_id": payment.charge_id,
+        }
+
+        response.append(payment_dict)
+
+    final_output = {
+        "status": "success",
+        "results": len(response),
+        "payments": response
+    }
+
+    return final_output
+
+
+def taxi_parking_serializer(payments: PaymentTaxi):
+    response = []
+
+    for payment in payments:
+
+        payment_dict = {
+            "id": str(payment.id),
+            "vehicle_id": payment.vehicle_id,
+            "charge_id": payment.charge_id,
+        }
+
+        response.append(payment_dict)
+
+    final_output = {
+        "status": "success",
+        "results": len(response),
+        "payments": response
+    }
+
+    return final_output
+
+
+def boda_parking_serializer(payments: PaymentBodaboda):
+    response = []
+
+    for payment in payments:
+
+        payment_dict = {
+            "id": str(payment.id),
+            "vehicle_id": payment.vehicle_id,
+            "charge_id": payment.charge_id,
+        }
+
+        response.append(payment_dict)
+
+    final_output = {
+        "status": "success",
+        "results": len(response),
+        "payments": response
+    }
+
+    return final_output
