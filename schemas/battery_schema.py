d7a6f58b741be77e6e3aae4e29bbb6668a6c8019
davidkaggulire1 <dkaggulire@gmail.com>
2022-09-13 01:15:54 +0300

    battery schema

---
+++ schemas/battery_schema.py
@@ -0,0 +1,22 @@
+"""charge_schema.py"""
+
+from marshmallow import Schema, fields, validate
+import string
+
+
+class BatterySectionSchema(Schema):
+    battery_size = fields.String(required=True, error_messages={
+        "required": "Battery_size is required."}, validate=[
+        validate.Length(min=3)])
+
+
+class BatteryPaymentSchema(Schema):
+    """schema for payment"""
+    battery_id = fields.String(required=True, error_messages={
+        "required": "battery_id is required."}, validate=[
+        validate.Length(equal=36)])
+    vehicle_id = fields.String(required=True, error_messages={
+        "required": "Vehicle_id is required."}, validate=[
+        validate.Length(equal=36)])
+    fee = fields.Integer(required=True, error_messages={
+        "required": "Fee is required."})
