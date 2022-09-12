328abf0c84518b31afdfc1722cd533afa9a8b2f4
davidkaggulire1 <dkaggulire@gmail.com>
2022-09-13 01:14:40 +0300

    edit schema

--- schemas/clinic_schema.py
+++ schemas/clinic_schema.py
@@ -4,15 +4,27 @@ from marshmallow import Schema, fields, validate
 import string
 
 
-class DurationSchema(Schema):
+class CarTyreClinicSchema(Schema):
     ALLOWED = string.ascii_letters
-    vehicle_type = fields.String(required=True, error_messages={
-                                 "required": "Duration is required."}, validate=[
-        validate.Length(min=3), validate.ContainsOnly(ALLOWED, error="only letters allowed")])
+    service = fields.String(required=True, error_messages={
+        "required": "Service is required."}, validate=[
+        validate.Length(min=3)])
+    fee = fields.Integer(required=True, error_messages={
+        "required": "Fee is required."})
 
 
 class CarTypeSchema(Schema):
     ALLOWED = string.ascii_letters
     type = fields.String(required=True, error_messages={
-                                 "required": "car type is required."}, validate=[
+        "required": "car type is required."}, validate=[
         validate.Length(min=3), validate.ContainsOnly(ALLOWED, error="only letters allowed")])
+
+
+class ClinicPaymentSchema(Schema):
+    """schema for payment"""
+    service_id = fields.String(required=True, error_messages={
+        "required": "Service is required."}, validate=[
+        validate.Length(equal=36)])
+    vehicle_id = fields.String(required=True, error_messages={
+        "required": "vehicle_id is required."}, validate=[
+        validate.Length(equal=36)])
