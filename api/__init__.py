903a00ad9ca15ba64842ca0f8e5247b845823678
davidkaggulire1 <dkaggulire@gmail.com>
2022-09-26 23:29:57 +0300

    add user routes

--- api/__init__.py
+++ api/__init__.py
@@ -4,6 +4,7 @@ from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from config import app_config
 from flask_migrate import Migrate
+from flask_bcrypt import Bcrypt
 
 import os
 
@@ -20,7 +21,8 @@ app = Flask(__name__)
 api = Api(app)
 app.config.from_object(app_config["testing"])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-CORS(app)   
+CORS(app)
+bcrypt = Bcrypt(app)   
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 # db.create_all()
@@ -31,6 +33,7 @@ from api.routes import charge_routes
 from api.routes import cartype_routes
 from api.routes import parking_routes
 from api.routes import clinic_routes, battery_routes, admin_routes
+from api.routes import user_routes
 
 
 
