b3ac7e36120fd134c3b63c07c32b5e19b33fc225
davidkaggulire1 <dkaggulire@gmail.com>
2022-09-28 00:15:53 +0300

    run flake8

--- schemas/charge_schema.py
+++ schemas/charge_schema.py
@@ -6,9 +6,9 @@ import string
 
 class ChargeSchema(Schema):
     ALLOWED = string.ascii_letters
-    duration = fields.String(required=True, error_messages={
-                                 "required": "Duration is required."}, validate=[
-        validate.Length(min=1)])
+    duration_required = {"required": "Duration is required."}
+    duration = fields.String(required=True, error_messages=duration_required,
+                             validate=[validate.Length(min=1)])
 
     charge = fields.Integer(required=True, error_messages={
         "required": "Charge is required."})
