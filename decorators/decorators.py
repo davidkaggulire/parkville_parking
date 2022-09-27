0d7bcb12f407d5b3df887a21169c2da519617a4b
davidkaggulire1 <dkaggulire@gmail.com>
2022-09-28 00:19:43 +0300

    add token required decorator

--- decorators/decorators.py
+++ decorators/decorators.py
@@ -1,9 +1,9 @@
 """decorators.py"""
 
-from flask import jsonify, request
 from marshmallow import ValidationError
 from functools import wraps
-from flask import jsonify,request, make_response
+from flask import jsonify, request, make_response
+from api.models import User
 
 
 def required_params(schema):
@@ -22,4 +22,38 @@ def required_params(schema):
             return fn(*args, **kwargs)
 
         return wrapper
+    return decorator
+
+
+def token_required(f):
+    @wraps(f)
+    def decorator(*args, **kwargs):
+        token = None
+
+        if "Authorization" in request.headers:
+            token = request.headers["Authorization"].split(" ")[1]
+
+        if not token:
+            return jsonify({'message': 'a valid token is missing'})
+
+        try:
+            data = User.decode_auth_token(token)
+            print(data)
+            if data == "Signature expired. Please log in again.":
+                message = {
+                    'message': 'Signature expired. Please log in again.'
+                }
+                return jsonify(message)
+            if data == "Token blacklisted. Please log in again.":
+                message = {
+                    'message': 'Token blacklisted. Please log in again.'
+                }
+                return jsonify(message)
+            print("hello")
+            current_user = User.query.filter_by(id=data).first()
+            print(current_user)
+        except Exception:
+            return jsonify({'message': 'token is invalid'})
+
+        return f(current_user)
     return decorator
