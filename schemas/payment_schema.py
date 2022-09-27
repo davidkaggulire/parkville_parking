f0eef0bd2ce096b2cf0a525429987e363229428d
davidkaggulire1 <dkaggulire@gmail.com>
2022-09-28 00:17:28 +0300

    run flake8

--- schemas/payment_schema.py
+++ schemas/payment_schema.py
@@ -4,7 +4,8 @@ from marshmallow import Schema, fields, validate
 
 
 class PaymentSchema(Schema):
-    vehicle_id = fields.String(required=True, error_messages={"required": "Driver_name is required."}, validate=[
-        validate.Length(equal=36)])
+    driver_name = {"required": "Driver_name is required."}
+    vehicle_id = fields.String(required=True, error_messages=driver_name,
+                               validate=[validate.Length(equal=36)])
     charge_id = fields.String(required=True, error_messages={
         "required": "Color is required."}, validate=validate.Length(equal=36))
