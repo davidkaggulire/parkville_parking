69f817375ab891777ce5168f3949386cda979e83
davidkaggulire1 <dkaggulire@gmail.com>
2022-09-28 00:20:57 +0300

    add user routes

--- api/__init__.py
+++ api/__init__.py
@@ -1,29 +1,25 @@
 from flask import Flask, jsonify
-from flask_restful import Api, Resource, reqparse
+from flask_restful import Api
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from config import app_config
 from flask_migrate import Migrate
 from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager
+from flask_jwt_extended import jwt_required
 
-import os
-
-# app = Flask(__name__)
-# api = Api(app)
-
-# CORS(app)
 # app.config.from_object(os.environ['APP_SETTINGS'])
 # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-# db = SQLAlchemy(app)
 
 app = Flask(__name__)
 api = Api(app)
 app.config.from_object(app_config["testing"])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 CORS(app)
-bcrypt = Bcrypt(app)   
+bcrypt = Bcrypt(app)
 db = SQLAlchemy(app)
+jwt = JWTManager(app)
 migrate = Migrate(app, db)
 # db.create_all()
 
@@ -34,8 +30,22 @@ from api.routes import cartype_routes
 from api.routes import parking_routes
 from api.routes import clinic_routes, battery_routes, admin_routes
 from api.routes import user_routes
+from api.models import BlacklistToken
 
 
+@jwt.token_in_blocklist_loader
+def check_if_token_revoked(jwt_header, jwt_payload: dict) -> bool:
+    jti = jwt_payload["jti"]
+    print("this is jti")
+    print(jti)
+    print(jwt_payload)
+    token = db.session.query(BlacklistToken.id).filter_by(token=jti).scalar()
+
+    return token is not None
 
 
-
+@app.route("/protected", methods=["GET"])
+@jwt_required()
+# @token_required
+def protected():
+    return jsonify(hello="world")
