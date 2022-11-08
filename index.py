f6979a84696a7617b1b860f70ee78ea5b9847657
davidkaggulire1 <dkaggulire@gmail.com>
2022-11-08 16:11:03 +0300

    index.py

--- index.py
+++ index.py
@@ -1,1 +1,7 @@
-from run import app
+"""run.py"""
+from api import app
+
+# app = create_app(os.environ.get('environment_variable')or 'testing')
+
+if __name__ == '__main__':
+    app.run(debug=True)
