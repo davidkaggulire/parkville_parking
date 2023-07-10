e028eb2ffb7bbe123621d2d4cfcec8e07b64c40a
davidkaggulire1 <dkaggulire@gmail.com>
2023-07-10 23:40:48 +0300

    enable different builds

--- run.py
+++ run.py
@@ -1,7 +1,8 @@
 """run.py"""
+import os
 from api import app
 
 # app = create_app(os.environ.get('environment_variable')or 'testing')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True)
