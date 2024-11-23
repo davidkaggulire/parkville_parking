864cacbd1645125bc7edcc1cab112eeb21f90319
davidkaggulire <dkaggulire@gmail.com>
2024-11-23 02:21:14 +0100

    edit file

--- wsgi.py
+++ wsgi.py
@@ -2,7 +2,7 @@
 import os
 from api import create_app
 
-app = create_app(os.environ.get('env_variable')or 'testing')
+# app = create_app(os.environ.get('env_variable')or 'testing')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
